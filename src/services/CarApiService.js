a8914bc980ada05a29f1bfdbd09c556c323a636c
Theo Foucher <theonicolas.foucher@gmail.com>
2025-02-17 00:25:39 +0100

    changer ip dans api

--- src/services/CarApiService.js
+++ src/services/CarApiService.js
@@ -4,7 +4,7 @@ class CarApiService {
     // Méthode pour récupérer toutes les marques via l'API
     static async getAllMakes() {
         try {
-            const response = await axios.get('http://localhost:3000/api/makes');
+            const response = await axios.get('http://0.0.0.0:3000/api/makes');
             return response.data;  // Retourne les marques récupérées
         } catch (error) {
             console.error('Erreur lors de la récupération des marques:', error);
@@ -15,7 +15,7 @@ class CarApiService {
     // Méthode pour récupérer les modèles d'une marque donnée via l'API
     static async getModelsForMake(makeId) {
         try {
-            const response = await axios.get(`http://localhost:3000/api/models/${makeId}`);
+            const response = await axios.get(`http://0.0.0.0:3000/api/models/${makeId}`);
             return response.data;  // Retourne les modèles récupérés pour cette marque
         } catch (error) {
             console.error(`Erreur lors de la récupération des modèles pour la marque ${makeId}:`, error);
@@ -26,7 +26,7 @@ class CarApiService {
     // Méthode pour récupérer une voiture aléatoire via l'API
     static async getRandomCar() {
         try {
-            const response = await axios.get('http://localhost:3000/api/random-car');
+            const response = await axios.get('http://0.0.0.0:3000/api/random-car');
             return response.data;  // Retourne la voiture aléatoire récupérée
         } catch (error) {
             console.error('Erreur lors de la récupération des données de la voiture:', error);
