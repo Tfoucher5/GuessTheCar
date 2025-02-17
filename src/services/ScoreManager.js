ebe6c0f1f872413d841d901351242bb037f5e70b
Theo Foucher <theonicolas.foucher@gmail.com>
2025-02-17 19:21:21 +0100

    fix 2 ScoreManager

--- src/services/ScoreManager.js
+++ src/services/ScoreManager.js
@@ -1,3 +1,7 @@
+const fs = require('fs').promises;
+const path = require('path');
+const UserStats = require('./UserStats');
+
 class ScoreManager {
     constructor() {
         this.stats = new Map();
