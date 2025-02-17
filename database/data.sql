f9b17cac34ef5af3aac2f1b8b60fdf0a2cf8f11b
Theo Foucher <theonicolas.foucher@gmail.com>
2025-02-17 15:37:05 +0100

    edit sql

--- database/data.sql
+++ database/data.sql
@@ -172,7 +172,6 @@ VALUES
     (1, 'C-HR', 2020, 1),
     (1, 'Avalon', 2019, 2),
     (1, 'Highlander', 2022, 2),
-
     -- Ford
     (2, 'Mustang', 2021, 3),
     (2, 'Focus', 2020, 1),
@@ -182,7 +181,6 @@ VALUES
     (2, 'Ranger', 2020, 2),
     (2, 'Edge', 2019, 2),
     (2, 'Escape', 2022, 1),
-
     -- BMW
     (3, 'M3', 2022, 3),
     (3, 'X5', 2021, 2),
@@ -192,7 +190,6 @@ VALUES
     (3, 'X3', 2020, 2),
     (3, 'X7', 2019, 2),
     (3, 'i8', 2022, 3),
-
     -- Peugeot
     (4, '208', 2023, 1),
     (4, '308', 2022, 1),
@@ -202,7 +199,6 @@ VALUES
     (4, 'RCZ', 2020, 2),
     (4, '607', 2019, 2),
     (4, 'Partner', 2022, 1),
-
     -- Ferrari
     (5, '488 GTB', 2018, 3),
     (5, 'F8 Tributo', 2021, 3),
@@ -212,7 +208,6 @@ VALUES
     (5, 'Enzo', 2020, 3),
     (5, 'Portofino', 2019, 3),
     (5, 'California', 2022, 3),
-
     -- Mercedes
     (6, 'Classe A', 2021, 1),
     (6, 'Classe C', 2020, 2),
@@ -222,7 +217,6 @@ VALUES
     (6, 'SL', 2020, 3),
     (6, 'AMG GT', 2019, 3),
     (6, 'SLS AMG', 2022, 3),
-
     -- Renault
     (7, 'Clio', 2023, 1),
     (7, 'Megane', 2021, 1),
@@ -232,7 +226,6 @@ VALUES
     (7, 'Scenic', 2020, 1),
     (7, 'Espace', 2019, 2),
     (7, 'Koleos', 2022, 2),
-
     -- Honda
     (8, 'Civic', 2022, 1),
     (8, 'Accord', 2021, 2),
@@ -242,7 +235,6 @@ VALUES
     (8, 'HR-V', 2020, 1),
     (8, 'Legend', 2019, 2),
     (8, 'Odyssey', 2022, 1),
-
     -- Audi
     (9, 'A3', 2022, 1),
     (9, 'A4', 2021, 2),
@@ -252,7 +244,6 @@ VALUES
     (9, 'A5', 2020, 2),
     (9, 'RS3', 2019, 3),
     (9, 'RS7', 2022, 3),
-
     -- Chevrolet
     (10, 'Camaro', 2021, 3),
     (10, 'Corvette', 2020, 3),
@@ -262,7 +253,6 @@ VALUES
     (10, 'Impala', 2020, 2),
     (10, 'Blazer', 2019, 2),
     (10, 'Equinox', 2022, 1),
-
     -- Volkswagen
     (11, 'Golf', 2022, 1),
     (11, 'Polo', 2021, 1),
@@ -272,7 +262,6 @@ VALUES
     (11, 'ID.4', 2020, 2),
     (11, 'T-Roc', 2019, 1),
     (11, 'Touareg', 2022, 2),
-
     -- Nissan
     (12, 'GT-R', 2022, 3),
     (12, '370Z', 2021, 3),
@@ -282,7 +271,6 @@ VALUES
     (12, 'Pathfinder', 2020, 2),
     (12, 'X-Trail', 2019, 2),
     (12, 'Leaf', 2022, 1),
-
     -- Porsche
     (13, '911', 2022, 3),
     (13, 'Cayenne', 2021, 2),
@@ -293,7 +281,6 @@ VALUES
     (13, 'Boxster', 2020, 3),
     (13, '718 Cayman', 2019, 3),
     (13, 'Carrera GT', 2022, 3),
-
     -- Lamborghini
     (14, 'Huracan', 2022, 3),
     (14, 'Aventador', 2021, 3),
@@ -303,7 +290,6 @@ VALUES
     (14, 'Sesto Elemento', 2020, 3),
     (14, 'Veneno', 2019, 3),
     (14, 'Revuelto', 2022, 3),
-
     -- Mitsubishi
     (15, 'Lancer Evolution', 2022, 3),
     (15, 'Outlander', 2021, 2),
@@ -313,7 +299,6 @@ VALUES
     (15, 'Colt', 2020, 1),
     (15, 'Mirage', 2019, 1),
     (15, 'L200', 2022, 2),
-
     -- Subaru
     (16, 'Impreza', 2022, 2),
     (16, 'WRX STI', 2021, 3),
@@ -323,7 +308,6 @@ VALUES
     (16, 'Ascent', 2020, 2),
     (16, 'Legacy', 2019, 2),
     (16, 'SVX', 2022, 2),
-
     -- Mazda
     (17, 'MX-5', 2022, 2),
     (17, 'RX-8', 2021, 3),
@@ -333,7 +317,6 @@ VALUES
     (17, '6', 2020, 2),
     (17, '2', 2019, 1),
     (17, 'Cosmo', 2022, 2),
-
     -- Jaguar
     (18, 'F-Type', 2022, 3),
     (18, 'XE', 2021, 2),
@@ -343,7 +326,6 @@ VALUES
     (18, 'E-Pace', 2020, 2),
     (18, 'F-Pace', 2019, 2),
     (18, 'Mark X', 2022, 2),
-
     -- Tesla
     (19, 'Model S', 2022, 2),
     (19, 'Model 3', 2021, 2),
@@ -353,7 +335,6 @@ VALUES
     (19, 'Cybertruck', 2020, 2),
     (19, 'Semi', 2019, 3),
     (19, 'Model U', 2022, 2),
-
     -- Bugatti
     (20, 'Chiron', 2022, 3),
     (20, 'Veyron', 2021, 3),
@@ -363,7 +344,6 @@ VALUES
     (20, 'Type 57', 2020, 3),
     (20, 'Galibier', 2019, 3),
     (20, 'Bolide', 2022, 3),
-
     -- Alfa Romeo
     (21, 'Giulia', 2022, 2),
     (21, 'Stelvio', 2021, 2),
@@ -372,7 +352,6 @@ VALUES
     (21, 'Brera', 2020, 2),
     (21, 'Giulietta', 2019, 2),
     (21, '33 Stradale', 2022, 3),
-
     -- Aston Martin
     (22, 'DB11', 2022, 3),
     (22, 'Vantage', 2021, 3),
@@ -380,7 +359,6 @@ VALUES
     (22, 'DBX', 2020, 2),
     (22, 'Vanquish', 2019, 3),
     (22, 'Valhalla', 2022, 3),
-
     -- Bentley
     (23, 'Continental GT', 2022, 3),
     (23, 'Bentayga', 2021, 2),
@@ -388,7 +366,6 @@ VALUES
     (23, 'Brooklands', 2020, 3),
     (23, 'Arnage', 2019, 3),
     (23, 'Flying Spur', 2022, 3),
-
     -- Citroën
     (24, 'C3', 2022, 1),
     (24, 'C5 Aircross', 2021, 2),
@@ -396,7 +373,6 @@ VALUES
     (24, 'DS7', 2020, 2),
     (24, 'C4 Picasso', 2019, 1),
     (24, 'C6', 2022, 2),
-
     -- Dacia
     (25, 'Duster', 2022, 1),
     (25, 'Sandero', 2021, 1),
@@ -404,7 +380,6 @@ VALUES
     (25, 'Spring', 2020, 1),
     (25, 'Dokker', 2019, 1),
     (25, 'Lodgy', 2022, 1),
-
     -- Dodge
     (26, 'Challenger', 2022, 3),
     (26, 'Durango', 2021, 2),
@@ -412,7 +387,6 @@ VALUES
     (26, 'Dart', 2020, 1),
     (26, 'Magnum', 2019, 2),
     (26, 'Viper', 2022, 3),
-
     -- Fiat
     (27, '500', 2022, 1),
     (27, 'Panda', 2021, 1),
@@ -420,63 +394,54 @@ VALUES
     (27, 'Uno', 2020, 1),
     (27, 'Strada', 2019, 1),
     (27, 'Doblo', 2022, 1),
-
     -- Genesis
     (28, 'G80', 2022, 2),
     (28, 'GV80', 2021, 2),
     (28, 'G90', 2020, 2),
     (28, 'G70', 2019, 2),
     (28, 'Essentia', 2022, 3),
-
     -- GMC
     (29, 'Yukon', 2022, 2),
     (29, 'Sierra', 2021, 2),
     (29, 'Canyon', 2020, 2),
     (29, 'Acadia', 2019, 2),
     (29, 'Envoy', 2022, 2),
-
     -- Hyundai
     (30, 'Tucson', 2022, 1),
     (30, 'Santa Fe', 2021, 2),
     (30, 'Ioniq 5', 2020, 2),
     (30, 'Veloster', 2019, 2),
     (30, 'Kona', 2022, 1),
-
     -- Infiniti
     (31, 'QX50', 2022, 2),
     (31, 'QX80', 2021, 2),
     (31, 'QX60', 2020, 2),
     (31, 'Q70', 2019, 2),
     (31, 'Q50', 2022, 2),
-
     -- Jeep
     (32, 'Wrangler', 2022, 2),
     (32, 'Grand Cherokee', 2021, 2),
     (32, 'Renegade', 2020, 1),
     (32, 'Commander', 2019, 2),
     (32, 'Cherokee', 2022, 2),
-
     -- Kia
     (33, 'Sorento', 2022, 2),
     (33, 'Sportage', 2021, 1),
     (33, 'Telluride', 2020, 2),
     (33, 'Stinger', 2019, 3),
     (33, 'Soul', 2022, 1),
-
     -- Lancia
     (34, 'Ypsilon', 2022, 1),
     (34, 'Delta', 2021, 2),
     (34, 'Thema', 2020, 2),
     (34, 'Stratos', 2019, 3),
     (34, 'Montecarlo', 2022, 3),
-
     -- Land Rover
     (35, 'Defender', 2022, 2),
     (35, 'Range Rover', 2021, 2),
     (35, 'Discovery', 2020, 2),
     (35, 'Evoque', 2019, 2),
     (35, 'Freelander', 2022, 2),
-
     -- Suite des marques de luxe et de sport
     -- Lexus
     (36, 'RX', 2022, 2),
@@ -484,95 +449,81 @@ VALUES
     (36, 'LS', 2020, 2),
     (36, 'UX', 2019, 1),
     (36, 'GX', 2022, 2),
-
     -- McLaren
     (40, '720S', 2022, 3),
     (40, 'Speedtail', 2021, 3),
     (40, 'Senna', 2020, 3),
     (40, 'Elva', 2019, 3),
     (40, 'Artura', 2022, 3),
-
     -- Pagani
     (43, 'Huayra', 2022, 3),
     (43, 'Zonda', 2021, 3),
     (43, 'Imola', 2020, 3),
     (43, 'R Revolución', 2019, 3),
-
     -- Koenigsegg
     (64, 'Jesko', 2022, 3),
     (64, 'Regera', 2021, 3),
     (64, 'Gemera', 2020, 3),
     (64, 'Agera RS', 2019, 3),
-
     -- Marques plus accessibles
     -- Polestar
     (44, 'Polestar 2', 2022, 2),
     (44, '1', 2021, 3),
     (44, '3', 2022, 2),
     (44, '4', 2023, 2),
-
     -- Rivian
     (46, 'R1T', 2022, 2),
     (46, 'R1S', 2021, 2),
     (46, 'EDV 700', 2020, 2),
-
     -- Marques de luxe traditionnelles
     -- Rolls-Royce
     (47, 'Phantom', 2022, 3),
     (47, 'Ghost', 2021, 3),
     (47, 'Cullinan', 2020, 3),
     (47, 'Wraith', 2019, 3),
-
     -- Lotus
     (38, 'Evora', 2022, 3),
     (38, 'Esprit', 2021, 3),
     (38, 'Elite', 2020, 3),
     (38, 'Europa', 2019, 3),
     (38, 'Eletre', 2022, 3),
-
     -- Mini
     (41, 'Cooper', 2022, 2),
     (41, 'Countryman', 2021, 2),
     (41, 'Paceman', 2020, 2),
     (41, 'Cabrio', 2019, 2),
     (41, 'Rocketman', 2022, 2),
-
     -- Marques spécialisées performance
     -- Caterham
     (56, 'Seven 170', 2022, 3),
     (56, 'Superlight R500', 2020, 3),
     (56, 'CSR', 2019, 3),
     (56, '310R', 2018, 3),
-
     -- Donkervoort
     (120, 'D8 GTO', 2019, 3),
     (120, 'D8 270 RS', 2016, 3),
     (120, 'D10', 2023, 3),
     (120, 'D8 235', 2014, 3),
-
     -- Marques plus rares
     -- DeLorean
     (60, 'DMC-12', 1981, 3),
     (60, 'Alpha5', 2023, 3),
     (60, 'BTTF Edition', 2022, 3),
     (60, 'Omega', 2024, 3),
-
     -- Marques émergentes électriques
     -- Lucid
     (67, 'Air', 2022, 2),
     (67, 'Gravity', 2023, 2),
     (67, 'Sapphire', 2024, 3),
     (67, 'Pure', 2025, 2),
-
     -- Fisker
     (61, 'Ocean', 2023, 2),
     (61, 'Karma', 2019, 3),
     (61, 'Pear', 2022, 2),
     (61, 'Ronin', 2025, 3),
-
     -- Dernières entrées spéciales
     -- GTA Motor
     (130, 'Spano', 2013, 3),
     (130, 'Spano R', 2016, 3),
     (130, 'Spano X', 2019, 3),
-    (130, 'Spano GT', 2023, 3),
+    (130, 'Spano GT', 2023, 3);
