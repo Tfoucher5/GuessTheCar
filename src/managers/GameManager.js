c112d76fa2476b631147065dee40e821b4f7e8fe
Theo Foucher <theonicolas.foucher@gmail.com>
2025-02-17 09:36:33 +0100

    rollback last commit (change public to private)

--- src/managers/GameManager.js
+++ src/managers/GameManager.js
@@ -98,7 +98,7 @@ class GameManager {
             // Création du thread pour la partie
             const thread = await interaction.channel.threads.create({
                 name: `🚗 Partie de ${interaction.user.username}`,
-                type: ChannelType.PrivateThread,
+                type: ChannelType.PublicThread,
                 autoArchiveDuration: 60
             });
     
